f9a53f2ce09c206363db053f65f888a3b2840ad5
DavidTsyg <davidtsyg@gmail.com>
2016-10-20 23:27:24 +0400

    Create init.cpp

---
+++ tests/source/init.cpp
@@ -0,0 +1,55 @@
+#include <Calculator.hpp>
+#include <catch.hpp>
+
+SCENARIO("Calculator sum", "[sum]") {
+    float a = 5;
+    float b = 4;
+    
+    double rv = sum( a, b );
+    
+	REQUIRE( rv == 9);
+}
+
+SCENARIO("Calculator sub", "[sub]") {
+    float a = 6;
+    float b = 4;
+    
+    double rv = sub( a, b );
+    
+	REQUIRE( rv == 2);
+}
+
+SCENARIO("Calculator multiply", "[multiply]") {
+    float a = 3;
+    float b = 4;
+    
+    double rv = multiply( a, b );
+    
+	REQUIRE( rv == 12);
+}
+
+SCENARIO("Calculator div", "[div]") {
+    float a = 8;
+    float b = 4;
+    
+    double rv = div( a, b );
+    
+	REQUIRE( rv == 2);
+}
+
+SCENARIO("Calculator pow", "[pow]") {
+    float a = 2;
+    int b = 5;
+    
+    double rv = pow( a, b );
+    
+	REQUIRE( rv == 32);
+}
+
+SCENARIO("Calculator sqrt", "[sqrt]") {
+    float a = 49;
+    
+    double rv = sqrt( a );
+    
+	REQUIRE( rv == 7);
+}
