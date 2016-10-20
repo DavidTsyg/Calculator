20a958801a4ecff45ce9204faeac5b41220af945
DavidTsyg <davidtsyg@gmail.com>
2016-10-20 23:05:54 +0400

    Create Calculator.cpp

---
+++ sources/Calculator.cpp
@@ -0,0 +1,30 @@
+double sum(float a, float b){
+    return a + b;
+}
+double sub(float a, float b){
+    return a - b;
+}
+double multiply(float a, float b){
+    return a*b;
+}
+double div(float a, float b){
+    return double(a/b);
+}
+double pow(float a, int b){
+    float new_a = 1;
+    int i =1;
+    for(i = 1; i <= b; i++){
+        new_a = new_a * a;
+    }
+    return new_a;
+}
+double sqrt(float a) {
+    double n = 1;
+    int i = 1;
+    for(i;i<a;i++)
+    {
+        if (pow(n,2) == a) break;
+        else (n = 0.5*(n + a/n));
+    }
+    return n;
+}
