e01c8cf1641c12b0c997248f0310a4f83036cb41
DavidTsyg <davidtsyg@gmail.com>
2016-11-11 02:56:27 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -46,3 +46,10 @@ SCENARIO("Calculator pow", "[pow1]") {
 	REQUIRE( rv == 32);
 }
 
+}
+ SCENARIO ("calc sqrt1", "[sqrt1]"){
+    float a = 1.44;
+    double long rv = sqrt1 (a);
+        REQUIRE ( rv == 1.2 );
+}
+
