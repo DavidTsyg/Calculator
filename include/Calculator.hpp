11e79a8fdd3fa60167e722d497ac8e10042b8c6a
DavidTsyg <davidtsyg@gmail.com>
2016-12-11 04:53:26 -0500

    Update Calculator.hpp

--- include/Calculator.hpp
+++ include/Calculator.hpp
@@ -1,6 +1,6 @@
 double sum(float a, float b);
 double sub(float a, float b);
 double multiply(float a, float b);
-double div(float a, float b);
+double *div(float a, float b);
 double pow1(float a, int b);
 double sqrt1(float a);
