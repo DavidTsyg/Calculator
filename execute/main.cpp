160334bded2c913378a374b6a8f3eceb0ed4a39d
DavidTsyg <davidtsyg@gmail.com>
2016-12-11 04:57:28 -0500

    Update main.cpp

--- execute/main.cpp
+++ execute/main.cpp
@@ -14,7 +14,7 @@ int main()
   cout << pow1(a, b) << endl;
   double *n = div(a,b);
   if (buf) cout << "Частное: " << *buf << endl;
-    else cout << "На ноль делить нельзя." << endl;
+  else cout << "На ноль делить нельзя." << endl;
   cout << multiply(a, b) << endl;
   cout << sum(a, b) << endl;
   cout << sub(a, b) << endl;
