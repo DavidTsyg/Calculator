28bcfac588104da75c923a5c7be0367fcff6e83a
DavidTsyg <davidtsyg@gmail.com>
2016-12-13 11:37:44 -0500

    Update Calculator.hpp

--- include/Calculator.hpp
+++ include/Calculator.hpp
@@ -3,4 +3,4 @@ double sub(double a, double b);
 double multiply(double a, double b);
 double *div(double a, double b);
 double pow1(double a, int b);
-double *sqrt1(double a);
+long double *sqrt1(double a);
