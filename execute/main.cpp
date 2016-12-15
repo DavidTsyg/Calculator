e4366e58b28f076f6b2e3cc0f5671cfb37128a2f
DavidTsyg <davidtsyg@gmail.com>
2016-12-15 09:37:42 -0500

    Update main.cpp

--- execute/main.cpp
+++ execute/main.cpp
@@ -1,39 +1,25 @@
-#include <iostream> 
-#include "calculator.hpp"
+#include <iostream>
+
+#include "Calculator.hpp"
+
 using namespace std;
 
-int main() {
-	int a, b;
-	cout << "Enter a,b:";
-	cin >>a;
-	cin >>b;
-	cout << "Sum:" << sum(a, b) << endl;
-	cout << "Division:" << div(a, b) << endl;
-	cout << "Multiplication:" << multiply(a, b) << endl;
-	try {
-		cout <<"Substraction:"<<sub(a,b)<<endl;
-	}
-	catch (char *Err)
-	{
-		cout << Err << endl;
-	}
-   try {
-		cout << "Pow a to b: " << pow1(a, b) << endl;
-	}
-	catch (int i)
-	{
-		cout << "Pow a to b:" << i << endl;
-	}
-	try{
-	cout << "Sqrt a: " << sqrt1(a) << endl;
-	}
-	catch (char *Errr)
-	{
-		cout<<Errr<<endl;
-	}
-	
-	return 0;
-
+int main() 
+{
+  double a, b;
+  
+  cin >> a >> b;
+    
+  long double *f = sqrt1(a);
+  if (f) cout << "Корень: " << *f << endl;
+  else cout << "Из отрицательного числа корень выделить нельзя." << endl;
+  cout << pow1(a, b) << endl;
+  delete f;
+  double *n = div(a,b);
+  if (n) cout << "Частное: " << *n << endl;
+  else cout << "На ноль делить нельзя." << endl;
+  delete n;
+  cout << multiply(a, b) << endl;
+  cout << sum(a, b) << endl;
+  cout << sub(a, b) << endl;
 }
-
-    Contact GitHub API Training Shop
