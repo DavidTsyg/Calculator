3006e47d5a460cd596d49d4050196ce8120c0d7c
DavidTsyg <davidtsyg@gmail.com>
2016-12-15 09:49:31 -0500

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -64,8 +64,7 @@ SCENARIO("Calculator pow.1", "[pow1]") {
 
 SCENARIO("calculator sqrt1.1", "[sqrt1]") {
     double a = -3;
-    long double *rv = new long double;
-    *rv = *sqrt1(a);
+    long double *rv = sqrt1(a);
     
 	REQUIRE( rv == nullptr );
     delete rv;
@@ -73,9 +72,8 @@ SCENARIO("calculator sqrt1.1", "[sqrt1]") {
 
 SCENARIO("Calculator div.1", "[div]") {
     float a = 8;
-    float b = 0;
-    double *rv = new double;	
-    *rv = *div( a, b );
+    float b = 0;	
+    double *rv = div( a, b );
     
 	REQUIRE( rv == nullptr );
     delete rv;
